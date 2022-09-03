ae43c064e03091e351f3bda4d9aac9831b755158
Neko Null <jerrylu.mail@qq.com>
2022-09-03 16:33:27 +0800

    feat: 增加 PWA 更新提示
    基于 https://dev.to/drbragg/handling-service-worker-updates-in-your-vue-pwa-1pip

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <GojuonQuiz/>
 
+  <div v-if="updateExists">
+    {{ t('update_available') }}
+    <button @click="refreshApp">
+      {{ t('update_button') }}
+    </button>
+  </div>
+
   <component :is="'script'">
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
@@ -12,17 +19,31 @@
 
 <script>
 import GojuonQuiz from './components/GojuonQuiz.vue'
+import update from './mixins/update'
+import { useI18n } from 'vue-i18n'
+
 
 export default {
   name: 'App',
   components: {
     GojuonQuiz,
   },
+  mixins: [update],
   mounted() {
     let gaScript = document.createElement('script')
     gaScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-FNEHZZBCRR')
     document.head.appendChild(gaScript)
   },
+  setup() {
+    const { t } = useI18n({
+        inheritLocale: true,
+        missingWarn: false,
+        fallbackWarn: false,
+        // useScope: 'local',
+    })
+
+    return { t }
+  }
 }
 </script>
 
