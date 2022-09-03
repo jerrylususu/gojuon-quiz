711165ba108709f804c1306efb36d518fbf26e7c
Neko Null <jerrylu.mail@qq.com>
2022-09-03 16:46:49 +0800

    fix: 修复构建后 path 错误 404 bug

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: './',
+  publicPath: './',
 
   pluginOptions: {
     i18n: {
